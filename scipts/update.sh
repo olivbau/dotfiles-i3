c93a805275e9be3516b428dcb5346bc2f8e44845
Baurain Olivier <baurain.o@gmail.com>
2017-10-18 15:03:05 +0200

    add scripts

---
+++ scipts/update.sh
@@ -0,0 +1,18 @@
+
+dotdir=~/Documents/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+
+# i3
+cp -f ~/.config/i3/config $dotdir/i3
+
+# compton
+cp -f ~/.config/compton.conf $dotdir/compton
+
+# polybar
+cp -f ~/.config/polybar/config $dotdir/polybar
+
+# xresources
+cp -f ~/.Xresources $dotdir/Xresources
+
+# zsh
+cp -f ~/.zshrc $dotdir/zsh
