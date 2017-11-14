4724a742b95bbcf66c31d01111b7d4bbeee1cde0
Baurain Olivier <baurain.o@gmail.com>
2017-11-14 18:32:09 +0100

    tray+multiscreen support

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,6 +1,11 @@
 killall polybar
 
 while pgrep -x polybar >/dev/null; do sleep 1; done
+monitors=$(xrandr -q| grep -n "connected"| wc -l)
+if [ $monitors = 1 ]; then
+	polybar single &
+else
+	polybar one &
+	polybar two &
+fi
 
-polybar one &
-polybar two &
